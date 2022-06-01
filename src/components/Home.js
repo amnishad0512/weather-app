ff28edd4f36871157778a05c0a988d4194e0cd25
Manoj <amnishad0512@gmail.com>
2022-06-01 23:46:32 +0530

    weather app completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,20 +2,54 @@ import React from "react";
 
 const Home = ({ detail }) => {
   return (
-    <div className="w-50 mx-auto my-4">
-        <div className="row">
-            <div className="col-3 bg-primary">a</div>
+    <div className="w-75 mx-auto">
+      <div className="row mb-5">
+        <div className="col-4 offset-4">
+          <img src={detail.img} alt="icon" /> <h2>{detail.weather}</h2>
         </div>
-      <h2>{detail.name}</h2>
-      <h2>{detail.region}</h2>
-      <h2>{detail.country}</h2>
-      <h2>{detail.lat}</h2>
-      <h2>{detail.lon}</h2>
-      <h2>{detail.time}</h2>
-      <h2>{detail.img}</h2>
-      <h2>{detail.weather}</h2>
-      <h2>{detail.temp_c}</h2>
-      <h2>{detail.temp_f}</h2>
+      </div>
+      <div className="row">
+        <div className="col-4">
+          <h5>Name</h5>
+          <h3>{detail.name}</h3>
+        </div>
+        <div className="col-4">
+          <h5>Region</h5>
+          <h3>{detail.region}</h3>
+        </div>
+        <div className="col-4">
+          <h5>Country</h5>
+          <h3>{detail.country}</h3>
+        </div>
+      </div>
+      <div className="row my-5">
+        <div className="col-4">
+          <h5>Fahrenheit</h5>
+          <h3>{detail.temp_f}</h3>
+        </div>
+        <div className="col-4">
+          <h5>Celsius </h5>
+          <h3>{detail.temp_c}</h3>
+        </div>
+        <div className="col-4">
+          <h5>Weather </h5>
+          <h3>{detail.weather}</h3>
+        </div>
+      </div>
+      <div className="row my-5">
+        <div className="col-4">
+          <h5>Latitude</h5>
+          <h3>{detail.lat}</h3>
+        </div>
+        <div className="col-4">
+          <h5>Longitude</h5>
+          <h3>{detail.lon}</h3>
+        </div>
+        <div className="col-4">
+          <h5>Local Date-Time</h5>
+          <h3>{detail.time}</h3>
+        </div>
+      </div>
     </div>
   );
 };
